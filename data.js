92d53be43d6b281bd699fe3afca2661f02a95040
Hella_538 <105759627+ElenaShamanova@users.noreply.github.com>
2022-12-19 17:29:00 +0300

    HomeWork for Workshop 10 is completed

---
+++ data.js
@@ -0,0 +1,135 @@
+const request = `[{
+    "activity": "Clean out your garage",
+    "type": "busywork",
+    "participants": 1,
+    "price": 0,
+    "link": "",
+    "key": "7023703",
+    "accessibility": 0.1
+    }, 
+    {
+        "activity": "Learn the NATO phonetic alphabet",
+        "type": "education",
+        "participants": 1,
+        "price": 0,
+        "link": "https://en.wikipedia.org/wiki/NATO_phonetic_alphabet",
+        "key": "6706598",
+        "accessibility": 0
+        }, 
+        {
+            "activity": "Try a food you don't like",
+            "type": "recreational",
+            "participants": 1,
+            "price": 0.1,
+            "link": "",
+            "key": "6693574",
+            "accessibility": 0.05
+            }, 
+            {
+                "activity": "Learn origami",
+                "type": "education",
+                "participants": 1,
+                "price": 0.2,
+                "link": "",
+                "key": "8394738",
+                "accessibility": 0.3
+                }, 
+                {
+                    "activity": "Listen to music you haven't heard in a while",
+                    "type": "music",
+                    "participants": 1,
+                    "price": 0.05,
+                    "link": "",
+                    "key": "4296813",
+                    "accessibility": 0.9
+                    }, 
+                    {
+                        "activity": "Explore the nightlife of your city",
+                        "type": "social",
+                        "participants": 1,
+                        "price": 0.1,
+                        "link": "",
+                        "key": "2237769",
+                        "accessibility": 0.32
+                        }, 
+                    {
+  "activity": "Volunteer and help out at a senior center",
+  "type": "charity",
+  "participants": 1,
+  "price": 0,
+  "link": "",
+  "key": "3920096",
+  "accessibility": 0.1
+},
+{
+    "activity": "Go stargazing",
+    "type": "relaxation",
+    "participants": 1,
+    "price": 0,
+    "link": "",
+    "key": "8832605",
+    "accessibility": 0.1
+    },
+    {
+        "activity": "Learn how to french braid hair",
+        "type": "education",
+        "participants": 1,
+        "price": 0,
+        "link": "",
+        "key": "8926492",
+        "accessibility": 0.1
+        },
+        {
+            "activity": "Clean out your closet and donate the clothes you've outgrown",
+            "type": "charity",
+            "participants": 1,
+            "price": 0,
+            "link": "",
+            "key": "9026787",
+            "accessibility": 0.1
+            },
+            {
+                "activity": "Create or update your resume",
+                "type": "busywork",
+                "participants": 1,
+                "price": 0,
+                "link": "",
+                "key": "9364041",
+                "accessibility": 0.1
+                },
+                {
+                    "activity": "Go for a walk",
+                    "type": "relaxation",
+                    "participants": 1,
+                    "price": 0,
+                    "link": "",
+                    "key": "4286250",
+                    "accessibility": 0.1
+                    },
+                    {
+                        "activity": "Look at pictures and videos of cute animals",
+                        "type": "relaxation",
+                        "participants": 1,
+                        "price": 0,
+                        "link": "",
+                        "key": "2565076",
+                        "accessibility": 1
+                        },
+                        {
+                            "activity": "Catch up with a friend over a lunch date",
+                            "type": "social",
+                            "participants": 2,
+                            "price": 0.2,
+                            "link": "",
+                            "key": "5590133",
+                            "accessibility": 0.15
+                            },
+                            {
+                                "activity": "Make homemade ice cream",
+                                "type": "cooking",
+                                "participants": 1,
+                                "price": 0.2,
+                                "link": "",
+                                "key": "3818400",
+                                "accessibility": 0.2
+                                }]`
